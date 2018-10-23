dcdebf9e8d623b4bb161a969c1637974c077fb91
Dan Meliza <dan@meliza.org>
2018-10-22 23:00:51 -0400

    work on spike sorting lab

--- tools/signal.py
+++ tools/signal.py
@@ -6,6 +6,7 @@ from __future__ import print_function, division, absolute_import
 import os
 import glob
 import ewave
+import numpy as np
 
 
 def load_stimulus(stimname):
@@ -19,11 +20,19 @@ def load_stimulus(stimname):
     return wavfile.read(), wavfile.sampling_rate
 
 
-def load_raw_responses(unit, stimname):
-    return [ewave.open(f).read() for f in glob.glob(os.path.join("data", "raw", "%s_%s*.wav" % (unit, stimname)))]
+def load_raw_responses(unit, stimname, offset=-2):
+    r = []
+    t = None
+    for i, f in enumerate(glob.glob(os.path.join("data", "raw", "%s_%s*.wav" % (unit, stimname)))):
+        with ewave.open(f) as fp:
+            r.append(fp.read())
+            if i == 0:
+                t = np.arange(0, fp.nframes / fp.sampling_rate, 1. / fp.sampling_rate) + offset
+    # assumes that all the responses are the same duration
+    return r, t
 
 
-def specgram(signal, nfft, shift, sampling_rate, compress):
+def specgram(signal, sampling_rate, nfft=256, shift=128, compress=1):
     """
     Calculate a spectrogram of signal.
 
