10aceae1b674967015cabcbff2a91a37db2ecb5e
Dan Meliza <dan@meliza.org>
2024-01-11 14:01:08 -0500

    update notebooks 0 and 1, fix some broken dependencies

--- comp_neurosci_uva/data.py
+++ comp_neurosci_uva/data.py
@@ -6,46 +6,57 @@ from __future__ import print_function, division, absolute_import
 import os
 import numpy as np
 
-e2_std = 0.2
-data_path = "data"
+from pathlib import Path
+from typing import Tuple, List
 
-def mult_error_data(mu, N):
-    """Generate N samples from a distribution with multiplicative error """
+import ewave
+
+e2_std = 0.2
+data_path = Path("data")
+
+
+def mult_error_data(mu: np.number, N: int) -> np.ndarray:
+    """Generate N samples from a distribution with multiplicative error"""
     from .dists import normal
+
     return mu * np.absolute(normal(1.0, e2_std).rvs(N)) / 5
 
 
-def load_timeseries(*path):
-    import ewave
-    wavfile = ewave.open(os.path.join(data_path, *path), "r")
+def load_timeseries(subdir: Path) -> Tuple[np.ndarray, int]:
+    wavfile = ewave.open(data_path / subdir, "r")
     return wavfile.read(memmap=False), wavfile.sampling_rate
 
 
-def load_raw_responses(*path, unit, stimname, offset=-2):
-    import ewave
-    import glob
+def load_raw_responses(
+    subdir: Path, *, unit: str, stimname: str, offset: float = -2
+) -> Tuple[List[np.ndarray], np.ndarray]:
     r = []
     t = None
-    for i, f in enumerate(glob.glob(os.path.join(data_path, *path, "%s_%s*.wav" % (unit, stimname)))):
+    path = data_path / subdir
+    for i, f in enumerate(path.glob(f"{unit}_{stimname}*.wav")):
         with ewave.open(f) as fp:
             r.append(fp.read())
             if i == 0:
-                t = np.arange(0, fp.nframes / fp.sampling_rate, 1. / fp.sampling_rate) + offset
+                t = (
+                    np.arange(0, fp.nframes / fp.sampling_rate, 1.0 / fp.sampling_rate)
+                    + offset
+                )
     # assumes that all the responses are the same duration
     return r, t
 
 
-def load_pprox(*path):
-    """ Loads pprox+json response data for unit and returns it in a Python dictionary """
+def load_pprox(subdir: Path):
+    """Loads pprox+json response data for unit and returns it in a Python dictionary"""
     import json
-    filename = os.path.join(data_path, *path) + ".json"
-    with open(filename, 'rU') as fp:
+
+    filename = data_path / subdir.with_suffix(".json")
+    with open(filename, "r") as fp:
         data = json.load(fp)
         return data
 
 
 try:
-    from comp_neurosci_uva.local_settings import *
+    from comp_neurosci_uva.local_settings import data_path
 except Exception as e:
     print(e)
     pass
--- comp_neurosci_uva/graphics.py
+++ comp_neurosci_uva/graphics.py
@@ -10,6 +10,7 @@ import numpy as np
 def plot_some_spikes(ax, waveforms, n_max=20, **kwargs):
     """Plot up to n_max waveforms in ax"""
     from numpy import random
+
     N = waveforms.shape[0]
     sample = random.choice(N, min(N, n_max), replace=False)
     ax.cla()
@@ -21,26 +22,33 @@ def spike_detector(ax1, ax2, data, time):
     import ipywidgets as ipyw
     import quickspikes as qs
 
-    osc = data.astype('d')
+    osc = data.astype("d")
 
     thresh = r_max = osc.max() * 1.2
-    thresh_w = ipyw.FloatSlider(value=thresh, min=0, max=r_max, step=r_max / 100,
-                                description="Threshold:", continuous_update=False)
+    thresh_w = ipyw.FloatSlider(
+        value=thresh,
+        min=0,
+        max=r_max,
+        step=r_max / 100,
+        description="Threshold:",
+        continuous_update=False,
+    )
     display(thresh_w)
 
     ax1.plot(time, osc)
-    line, = ax1.plot((time[0], time[-1]), (thresh, thresh), 'k')
-    points, = ax1.plot([], [], 'ro')
+    (line,) = ax1.plot((time[0], time[-1]), (thresh, thresh), "k")
+    (points,) = ax1.plot([], [], "ro")
 
     def update(thresh):
         from quickspikes.tools import filter_times
+
         line.set_ydata((thresh, thresh))
         det = qs.detector(thresh, 40)
         spike_t = filter_times(det.send(osc), 10, osc.size - 20)
         if spike_t:
             spike_t = np.asarray(spike_t)
-            spike_w = np.asarray(qs.peaks(osc, spike_t, 10, 20))
-            plot_some_spikes(ax2, spike_w, color='k', alpha=0.2)
+            spike_w = np.asarray(qs.peaks(osc, spike_t, n_before=10, n_after=20))
+            plot_some_spikes(ax2, spike_w, color="k", alpha=0.2)
             points.set_xdata(time[spike_t])
             points.set_ydata(osc[spike_t])
         ax1.figure.canvas.draw()
@@ -61,25 +69,34 @@ def spectrogram_explorer(ax, signal):
 
     NFFT = ipyw.IntSlider(value=256, min=8, max=1024, step=1, description="NFFT:")
     shift = ipyw.IntSlider(value=128, min=8, max=1024, step=1, description="shift:")
-    compress = ipyw.FloatLogSlider(value=0, base=10, min=-2, max=5, step=0.2, description="compress:")
+    compress = ipyw.FloatLogSlider(
+        value=0, base=10, min=-2, max=5, step=0.2, description="compress:"
+    )
     thresh = r_max = osc.max() * 1.2
-    thresh_w = ipyw.FloatSlider(value=thresh, min=0, max=r_max, step=r_max / 100,
-                                description="Threshold:", continuous_update=False)
+    thresh_w = ipyw.FloatSlider(
+        value=thresh,
+        min=0,
+        max=r_max,
+        step=r_max / 100,
+        description="Threshold:",
+        continuous_update=False,
+    )
     display(thresh_w)
 
     ax1.plot(time, osc)
-    line, = ax1.plot((time[0], time[-1]), (thresh, thresh), 'k')
-    points, = ax1.plot([], [], 'ro')
+    (line,) = ax1.plot((time[0], time[-1]), (thresh, thresh), "k")
+    (points,) = ax1.plot([], [], "ro")
 
     def update(thresh):
         from quickspikes.tools import filter_times
+
         line.set_ydata((thresh, thresh))
         det = qs.detector(thresh, 40)
         spike_t = filter_times(det.send(osc), 10, osc.size - 20)
         if spike_t:
             spike_t = np.asarray(spike_t)
             spike_w = np.asarray(qs.peaks(osc, spike_t, 10, 20))
-            plot_some_spikes(ax2, spike_w, color='k', alpha=0.2)
+            plot_some_spikes(ax2, spike_w, color="k", alpha=0.2)
             points.set_xdata(time[spike_t])
             points.set_ydata(osc[spike_t])
         ax1.figure.canvas.draw()
